01b1734e91d7100bc3444715c8bc4dc998114aa8
Daksh Meshram <78013164+Daksh-ai@users.noreply.github.com>
2021-06-30 12:12:19 +0530

    Add files via upload

---
+++ string swapcase.py
@@ -0,0 +1,7 @@
+def swapcase(string):
+    return s.swapcase()
+s=input("Enter The String")
+sub=swapcase(s)
+print(sub)
+#example-->input=Daksh output-->dAKSH
+#in genrally we say that its used to change the case of string and vice-versa
